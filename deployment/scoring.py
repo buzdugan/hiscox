1fdb270e6af419d6cd4d731ccfbb67fb2ff88dbb
“buzdugan” <andradaorbai@yahoo.com>
2025-06-26 16:16:31 +0100

    Score data with model from AWS without downloading it

--- deployment/scoring.py
+++ deployment/scoring.py
@@ -5,30 +5,11 @@ import os
 from pathlib import Path
 from datetime import datetime, timedelta
 
-import boto3
 import pandas as pd
 import mlflow
 from mlflow.tracking import MlflowClient
 # from prefect import flow, task
 # from prefect_aws import S3Bucket
-
-
-def download_s3_folder(bucket_name, s3_folder, local_dir):
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(bucket_name)
-
-    # Ensure the local directory exists
-    os.makedirs(local_dir, exist_ok=True)
-
-    for obj in bucket.objects.filter(Prefix=s3_folder):
-        target = os.path.join(local_dir, os.path.relpath(obj.key, s3_folder))
-        
-        # Create local dirs if needed
-        os.makedirs(os.path.dirname(target), exist_ok=True)
-
-        if not obj.key.endswith('/'):  # skip directories
-            print(f"Downloading {obj.key} to {target}")
-            bucket.download_file(obj.key, target)
 
 
 #@task(name="create_daily_data", log_prints=True)
@@ -55,7 +36,6 @@ def read_dataframe(file_path):
     return df
 
 
-# Version 2 - get production model run_id from registry
 #@task(name="get_production_model", log_prints=True)
 def get_prod_model(client, model_name):
     # Get all registered models for model name
@@ -81,10 +61,9 @@ def get_prod_model(client, model_name):
 
 
 #@task(name="load_model", log_prints=True)
-def load_model(run_id):
-    # prod_model = f's3://mlflow-artifacts-remote-hiscox/1/models/{run_id}/artifacts/'
-    # prod_model = f'mlartifacts/2/models/{run_id}/artifacts/'
-    prod_model = 'artifacts_aws/'
+def load_model(model_id, experiment_id):
+    prod_model = f's3://mlflow-artifacts-remote-hiscox/{experiment_id}/models/{model_id}/artifacts/'
+    print(f"Loading model from {prod_model}...")
     model = mlflow.pyfunc.load_model(prod_model)
     return model
 
@@ -111,6 +90,8 @@ def score_claim_status():
 
     experiment_name = "claims_status"
     model_name = f"{experiment_name}_classifier"
+    experiment_id = client.get_experiment_by_name(experiment_name).experiment_id
+    print(f"Experiment ID for {experiment_name}: {experiment_id}")
 
     # s3_bucket_block = S3Bucket.load("mlops-s3-bucket")
     # s3_bucket_block.download_folder_to_path(from_folder="data", to_folder="data")
@@ -132,16 +113,8 @@ def score_claim_status():
     print(f"Getting production model from registry...")
     run_id, model_id = get_prod_model(client, model_name)
         
-    print(f"Downloading model with run_id = {run_id} and model_id = {model_id}...")
-    download_s3_folder(
-        bucket_name='mlflow-artifacts-remote-hiscox',
-        s3_folder=f"1/models/{model_id}/artifacts/",
-        local_dir="artifacts_aws"
-    )
-    print(f"Downloaded model with run_id = {run_id} and model_id = {model_id}")
-
-    print(f"Loading model with run_id = {run_id}...")
-    model = load_model(run_id)
+    print(f"Loading model with model_id = {model_id}...")
+    model = load_model(model_id, experiment_id)
 
     print(f"Scoring the data using model with run_id = {run_id}...")
     apply_model(model, run_id, df, output_file_path)
