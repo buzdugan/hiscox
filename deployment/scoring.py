a59b595b3dc765a519817cb277ab83e0672a4812
“buzdugan” <andradaorbai@yahoo.com>
2025-06-26 16:04:22 +0100

    Add docker file and scoring that works on docker container by downloading the model from AWS

--- deployment/scoring.py
+++ deployment/scoring.py
@@ -5,14 +5,33 @@ import os
 from pathlib import Path
 from datetime import datetime, timedelta
 
+import boto3
 import pandas as pd
 import mlflow
 from mlflow.tracking import MlflowClient
-from prefect import flow, task
-from prefect_aws import S3Bucket
+# from prefect import flow, task
+# from prefect_aws import S3Bucket
 
 
-@task(name="create_daily_data", log_prints=True)
+def download_s3_folder(bucket_name, s3_folder, local_dir):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+
+    # Ensure the local directory exists
+    os.makedirs(local_dir, exist_ok=True)
+
+    for obj in bucket.objects.filter(Prefix=s3_folder):
+        target = os.path.join(local_dir, os.path.relpath(obj.key, s3_folder))
+        
+        # Create local dirs if needed
+        os.makedirs(os.path.dirname(target), exist_ok=True)
+
+        if not obj.key.endswith('/'):  # skip directories
+            print(f"Downloading {obj.key} to {target}")
+            bucket.download_file(obj.key, target)
+
+
+#@task(name="create_daily_data", log_prints=True)
 def create_daily_data(file_path, output_path):
     df = pd.read_csv(file_path)
     df.drop(columns=['claim_status'], inplace=True)
@@ -20,7 +39,7 @@ def create_daily_data(file_path, output_path):
     df.head(10).to_csv(output_path, index=False)
 
 
-@task(name="read_data", retries=3, retry_delay_seconds=2)
+#@task(name="read_data", retries=3, retry_delay_seconds=2)
 def read_dataframe(file_path):
     df = pd.read_csv(file_path)
     
@@ -37,39 +56,40 @@ def read_dataframe(file_path):
 
 
 # Version 2 - get production model run_id from registry
-@task(name="get_production_model", log_prints=True)
+#@task(name="get_production_model", log_prints=True)
 def get_prod_model(client, model_name):
     # Get all registered models for model name
     reg_models = client.search_registered_models(
         filter_string=f"name='{model_name}'"
     )
 
-    # Get production model run id
+    # Get production model run id and model id
     prod_model_run_id = None
+    prod_model_model_id = None
     for reg_model in reg_models:
         for model_version in reg_model.latest_versions:
             if model_version.current_stage == 'Production':
                 prod_model_run_id = model_version.run_id
+                prod_model_model_id = model_version.source.replace('models:/', '') 
                 break
 
     if prod_model_run_id:
         print(f"Production model run_id for {model_name}: {prod_model_run_id}")
-        return prod_model_run_id
+        return prod_model_run_id, prod_model_model_id
     else:   
         print(f"No production model found for {model_name}.")
 
 
-# Version 1 - we know the model run_id
-@task(name="load_model", log_prints=True)
+#@task(name="load_model", log_prints=True)
 def load_model(run_id):
     # prod_model = f's3://mlflow-artifacts-remote-hiscox/1/models/{run_id}/artifacts/'
     # prod_model = f'mlartifacts/2/models/{run_id}/artifacts/'
-    prod_model = f'artifacts_aws/'
+    prod_model = 'artifacts_aws/'
     model = mlflow.pyfunc.load_model(prod_model)
     return model
 
 
-@task(name="apply_model", log_prints=True)
+#@task(name="apply_model", log_prints=True)
 def apply_model(model, run_id, df, output_path):
 
     df['predicted_claim_status'] = model.predict(df)
@@ -79,25 +99,18 @@ def apply_model(model, run_id, df, output_path):
     df.to_csv(output_path, index=False)
 
 
-@flow(name="claim_status_scoring_flow", log_prints=True)
+#@flow(name="claim_status_scoring_flow", log_prints=True)
 def score_claim_status():
 
-    # Version 1
-    # RUN_ID = os.getenv('RUN_ID', "m-a2dd0166170844ecab99d852d6ce412d") # model in S3 bucket
-    # RUN_ID = "m-9eead17988824cac85cb40c965964150"  # model locally downloaded
-
-    # Version 2
-    # print("Loading aws profile...")
-    # os.environ["AWS_PROFILE"] = "mlops-user"  # AWS profile name
-    # tracking_server_host = "ec2-13-218-161-214.compute-1.amazonaws.com" # public DNS of the EC2 instance
-    # mlflow_tracking_uri = f"http://{tracking_server_host}:5000"
-    # mlflow_tracking_uri = "http://127.0.0.1:5000" # run locally
-    mlflow_tracking_uri = "sqlite:///mlflow.db"
-    experiment_name = "claims_status"
-    model_name = f"{experiment_name}_classifier"
-
+    print("Loading aws profile...")
+    os.environ["AWS_PROFILE"] = "mlops-user"  # AWS profile name
+    tracking_server_host = "ec2-3-95-217-242.compute-1.amazonaws.com" # public DNS of the EC2 instance
+    mlflow_tracking_uri = f"http://{tracking_server_host}:5000"
     print("Connecting to mlflow registry server...")
     client = MlflowClient(mlflow_tracking_uri)
+
+    experiment_name = "claims_status"
+    model_name = f"{experiment_name}_classifier"
 
     # s3_bucket_block = S3Bucket.load("mlops-s3-bucket")
     # s3_bucket_block.download_folder_to_path(from_folder="data", to_folder="data")
@@ -117,12 +130,20 @@ def score_claim_status():
     df = read_dataframe(yesterday_input_file_path) 
 
     print(f"Getting production model from registry...")
-    run_id = get_prod_model(client, model_name)
+    run_id, model_id = get_prod_model(client, model_name)
+        
+    print(f"Downloading model with run_id = {run_id} and model_id = {model_id}...")
+    download_s3_folder(
+        bucket_name='mlflow-artifacts-remote-hiscox',
+        s3_folder=f"1/models/{model_id}/artifacts/",
+        local_dir="artifacts_aws"
+    )
+    print(f"Downloaded model with run_id = {run_id} and model_id = {model_id}")
 
-    print(f"Loading model with run_id: {run_id}...")
+    print(f"Loading model with run_id = {run_id}...")
     model = load_model(run_id)
 
-    print(f"Scoring the data using model with run_id: {run_id}...")
+    print(f"Scoring the data using model with run_id = {run_id}...")
     apply_model(model, run_id, df, output_file_path)
     print(f"Scored the data.")
 
